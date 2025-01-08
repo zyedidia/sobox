926288d50d38aa21f8a3a423f46273f38314a984
Zachary Yedidia <zyedidia@gmail.com>
2025-01-08 12:38:04 -0800

    Add arm cbtrampolines

--- embed/arm64/cbtrampolines.s
+++ embed/arm64/cbtrampolines.s
@@ -1,5 +1,36 @@
+#define REG_BASE  x21
+#define REG_SYS   x25
+
 .text
 
 .global sbx_cbtrampoline
 sbx_cbtrampoline:
-	brk #0
+	// Entrypoint for callbacks. Callback target is in x10.
+	mrs	x11, tpidr_el0
+	add	x11, x11, #:tprel_hi12:lfi_myproc, lsl #12
+	add	x11, x11, #:tprel_lo12_nc:lfi_myproc
+	ldr	x11, [x11]
+
+	// load stack
+	ldr x12, [x11]
+	// save stack
+	mov x13, sp
+	str x13, [x11]
+	mov sp, x12
+
+	blr x10
+
+	mrs	x11, tpidr_el0
+	add	x11, x11, #:tprel_hi12:lfi_myproc, lsl #12
+	add	x11, x11, #:tprel_lo12_nc:lfi_myproc
+	ldr	x11, [x11]
+
+	// load stack
+	ldr x12, [x11]
+	// save stack
+	mov x13, sp
+	str x13, [x11]
+	mov sp, x12
+
+	add x30, REG_BASE, w30, uxtw
+	ret
